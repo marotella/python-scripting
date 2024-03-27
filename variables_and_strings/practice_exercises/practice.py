d3498fdd6ced357af7567bcb40c1338768168acb
marotella <michelle.a.rotella@gmail.com>
2024-03-26 20:35:43 -0400

    Practice 2 completed strings and variables

--- variables_and_strings/practice_exercises/practice.py
+++ variables_and_strings/practice_exercises/practice.py
@@ -6,3 +6,9 @@ print("Here is an animal, a vegetable, and a mineral.")
 print(animal)
 print(vegetable)
 print(mineral)
+
+
+user_input = input("PLease type something and presss enter: ")
+
+print("You entered: ")
+print(user_input)
