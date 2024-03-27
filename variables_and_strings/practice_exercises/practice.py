39651776323d3969023b169e7c7becbaafadf9ad
marotella <michelle.a.rotella@gmail.com>
2024-03-26 20:39:40 -0400

    practice 3 and strings and variables completed

--- variables_and_strings/practice_exercises/practice.py
+++ variables_and_strings/practice_exercises/practice.py
@@ -12,3 +12,15 @@ user_input = input("PLease type something and presss enter: ")
 
 print("You entered: ")
 print(user_input)
+
+cat_input = input("What does the cat say?")
+
+cat_length = len(cat_input)
+
+print('         {}'.format("_"*cat_length))
+print('         <{}>'.format(cat_input))
+print('         {}'.format("_"*cat_length))
+print('          /')
+print(' /\_/\   /')
+print('( o.o )')
+print(' > ^ <')
