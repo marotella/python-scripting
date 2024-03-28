d087b451979a7692f96631bf7f8b1ddacb253bb4
marotella <michelle.a.rotella@gmail.com>
2024-03-28 19:52:31 -0400

    functions completed

---
+++ functions/practice_exercises/practice.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+def createStory(adjective, noun , verb):
+    story = "The " + adjective + noun + verb
+    print(story)
+    
+    
+def getElements():
+    adjective = input("Enter an adjective: ")
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    createStory(adjective, noun, verb)
+    
+
+getElements()
