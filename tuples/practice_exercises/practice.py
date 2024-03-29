f7ccd6fe2aa43887a2ec6bb0a4539bf5a913e674
marotella <michelle.a.rotella@gmail.com>
2024-03-28 20:57:30 -0400

    Tuples completed

---
+++ tuples/practice_exercises/practice.py
@@ -0,0 +1,9 @@
+airports = [
+    ("O’Hare International Airport", 'ORD'),
+    ('Los Angeles International Airport', 'LAX'),
+    ('Dallas/Fort Worth International Airport', 'DFW'),
+    ('Pittsburgh International Airport', 'PIT')
+]
+
+for (airport, code) in airports:
+    print('The code for {} is {}.'.format(airport, code))
