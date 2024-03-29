79ec641e56b29fe42f2db4dd0b76878d682ace29
marotella <michelle.a.rotella@gmail.com>
2024-03-28 20:42:46 -0400

    dictionaries completed

---
+++ dictionaries/practice_exercises/practice.py
@@ -0,0 +1,11 @@
+fun_facts = {
+    "Lou" : "Loves tunas.",
+    "Stephen": "Loves Lou.",
+    "Michelle": "Loves Lou,",
+}
+
+fun_facts["Lou"] = "Loves tuna and pets."
+
+fun_facts["Tater"] = "Is missed so much"
+
+print(fun_facts)
